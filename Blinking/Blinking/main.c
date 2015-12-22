82d92fdf09c39052eecc4548452596f2956c0af8
Duc Cuong Dinh <cuongland.mobi@gmail.com>
2015-12-22 20:23:53 +0700

    init

---
+++ Blinking/Blinking/main.c
@@ -0,0 +1,18 @@
+/*
+ * Blinking.c
+ *
+ * Created: 12/22/2015 8:20:59 PM
+ * Author : Duc Cuong Dinh
+ */ 
+
+#include <avr32/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
